73c8472a493cca7f119b6a4cf0df6770fce3cdb5
Rike-Benjamin Schuppner <debilski@debilski.de>
2023-09-02 11:35:00 +0300

    API update Pelita 2.3

--- src/lib.rs
+++ src/lib.rs
@@ -11,15 +11,27 @@ pub type Pos = (usize, usize);
 #[derive(FromPyObject)]
 pub struct Bot {
     pub position: Pos,
+    #[pyo3(attribute("_initial_position"))]
+    pub initial_position: Pos,
     pub legal_positions: Vec<Pos>,
+    #[pyo3(from_py_with = "vectohashset")]
     pub walls: HashSet<Pos>,
     pub enemy: Vec<EnemyBot>,
     pub other: OtherBot,
     pub food: Vec<Pos>,
     pub shape: Shape,
     pub is_blue: bool,
+    pub turn: usize,
+    pub score: usize,
+    pub round: usize,
     #[pyo3(from_py_with = "emptyonce")]
     pub _say: OnceCell<String>
+}
+
+
+fn vectohashset(var: &PyAny) -> PyResult<HashSet<Pos>> {
+    let walls: Vec<Pos> = var.extract()?;
+    Ok(walls.into_iter().collect())
 }
 
 fn emptyonce(_var: &PyAny) -> PyResult<OnceCell<String>> {
@@ -37,37 +49,55 @@ impl Bot {
 #[derive(FromPyObject)]
 pub struct OtherBot {
     pub position: Pos,
+    #[pyo3(attribute("_initial_position"))]
+    pub initial_position: Pos,
     pub legal_positions: Vec<Pos>,
     pub is_blue: bool,
+    pub turn: usize,
+    pub score: usize,
 }
 
 #[derive(FromPyObject)]
 pub struct EnemyBot {
     pub position: Pos,
+    #[pyo3(attribute("_initial_position"))]
+    pub initial_position: Pos,
     pub is_noisy: bool,
     pub legal_positions: Vec<Pos>,
     pub food: Vec<Pos>,
     pub is_blue: bool,
+    pub turn: usize,
+    pub score: usize,
 }
 
 #[macro_export]
 macro_rules! pelita_player {
-    ($module_name: ident, $team_name: expr, $move_fn: expr) => {
+    ($module_name: ident, $team_name: expr, $move_fn: expr, $state_t:ty) => {
 
         use pyo3::prelude::*;
 
+        #[macro_use]
+        extern crate lazy_static;
+
         #[pyfunction]
         fn r#move(mut pybot: PyObject, state: PyObject) -> PyResult<Pos> {
+
+            use std::sync::Mutex;
+            lazy_static! {
+                static ref STATE: Mutex<Option<$state_t>> = Mutex::new(None);
+            }
+
             // ignoring the dict state that is passed from Pelita
             Python::with_gil(|py| {
 
                 let rustbot: Result<Bot, _> = pybot.extract(py);
+                let mut LOCKED_STATE = STATE.lock().unwrap();
                 match rustbot {
                     Ok(b) => {
-                        let result = $move_fn(&b, state);
+                        let result = $move_fn(&b, &mut *LOCKED_STATE);
 
                         if let Some(text) = b._say.get() {
-                            pybot.setattr(py, "_say", text);
+                            pybot.setattr(py, "_say", text)?;
                         }
 
                         Ok(result)
